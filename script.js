792ee58e806004b07bee39619f2997dca18773b3
George <cortes.george93@icloud.com>
2023-12-22 19:17:31 -0800

    Computer Choice bugfix

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3)  // get the computer choice of 0,1,2
+    console.log(choice)
     if(choice == 0){
         return "rock";
     }else{
-        choice == 1? "scissors" : "paper"
+        return choice == 1 ? "scissors" : "paper"
     }
 }
