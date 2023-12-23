79f4f1a67df1d12710f3f8e15536fd5f7085b262
George <cortes.george93@icloud.com>
2023-12-22 19:30:51 -0800

    added Play Round function

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function getComputerChoice(){
         return choice == 1 ? "scissors" : "paper"
     }
 }
+
+//Play a round
+function playRound(compSelection, playerSelection){
+    // you choose rock
+    if(compSelection == playerSelection){
+        return console.log("TIE")
+    }else if(playerSelection == 0){
+        return compSelection == 1? console.log("YOU WIN") : console.log("YOU LOSE");
+    }else if(playerSelection == 1){
+        return compSelection == 2 ? console.log("YOU WIN") : console.log("YOU LOSE")
+    }else{
+        return compSelection == 0 ? console.log("YOU WIN") : console.log("YOU LOSE")
+    }
+}
