2c9fcc0de100120257c3e88d2fdbd3729d0c82b0
George <cortes.george93@icloud.com>
2023-12-25 01:04:42 -0800

    added functioning buttons

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let scissorChoice = document.getElementById("games-s") 
+let rockChoice = document.getElementById("games-r")
+let paperChoice = document.getElementById("games-p")
+
+let s= document.querySelector("#games-s")
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3)  // get the computer choice of 0,1,2
     console.log(choice)
@@ -40,3 +45,33 @@ function playGame(){
         choice = prompt().toLowerCase();
     }
 }
+
+function resultPrompt(choice){
+    let res = document.getElementById("result")
+    let h3 = document.createElement("h3");
+    if(choice === -1){
+        h3.innerHTML = "TIE"
+    }else{
+        choice === 0 ? h3.innerHTML = "LOSE" : h3.innerHTML = "WIN"
+    }
+    res.appendChild(h3)
+}
+scissorChoice.addEventListener('click',()=>{
+    
+    let opt = playRound("scissors", getComputerChoice())
+    let container = document.getElementById("#game-c")
+    resultPrompt(opt)
+})
+
+rockChoice.addEventListener('click',()=>{
+    
+    let opt = playRound("rock", getComputerChoice())
+    let container = document.getElementById("#game-c")
+    resultPrompt(opt)
+})
+paperChoice.addEventListener('click',()=>{
+    
+    let opt = playRound("paper", getComputerChoice())
+    let container = document.getElementById("#game-c")
+    resultPrompt(opt)
+})
